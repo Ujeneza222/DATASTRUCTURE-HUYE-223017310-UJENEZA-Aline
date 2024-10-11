778a2cd1680e0a2b57c48024041d8f5671f6fbef
Ujeneza222 <ujenezaaline07@gmail.com>
2024-10-11 11:48:07 +0200

    Add files via upload
    assignment.py

---
+++ Aline.py
@@ -0,0 +1,81 @@
+stack_art=[]
+stack_exibit=[]
+def push_art():
+    if stack_art:
+        print(f"cartoons{stack_art}")
+        return  stack_art
+    
+def pop_art():
+    if stack_art:
+        print(f"cartoons{stack_art}")
+
+queue_art=[]
+queue_exhibit=[]
+
+def queue_request(art,exhibit):
+    if queue_art==[art]:
+        print(f"cartoons{art}")
+        queue_art.append(art)
+    elif queue_request(art ,exhibit):
+        print(f"Dolls{art}")
+    else:
+        print("nothing code")    
+        
+        
+queue_request("cartoons","cartoons")
+queue_request("dolls")
+queue_request("caartoons","cartoons")        
+stack_art=[]
+stack_exhibit=[]
+
+def push_art(art,exhibit):
+    if stack_art=="cartoons":
+        stack_art.append(art)
+        print(f"cartoons{art} to {exhibit}")
+        return stack_art
+def pop_art():
+    if stack_art=="Dolls":
+        stack_exhibit.append()
+        print(f"dolls{stack_art}")
+        push_art("star", "star")
+push_art("volcono", "volcono")
+push_art("ritco", "ritco")
+
+
+pop_art("star")
+pop_art("volcono")
+pop_art("ritco")            
+        
+art=[
+    
+"Artificial flowers andplants"
+"Bumper stickers"
+" decals"
+"stickers"
+"Cartographic works"
+"Cartoons"
+"comic strips"
+"Collages"
+]
+def list_request(art):
+    if list=="artifical flower and plant":
+        print("artificial flowers and plant")
+    elif  list=="bumper stricker":
+         print("bumper stricker")
+    elif list==  "decals" :
+        print("decals")  
+    elif list=="strickers" :
+        print("stickers")
+    else:
+       print ("Artificial flowers andplants" "Bumper stickers" " decals""stickers" "Cartographic works"
+"Cartoons"
+"comic strips"
+"Collages")     
+        
+list_request("Artificial flowers andplants")
+list_request("Bumper stickers")
+list_request(" decals")
+list_request("stickers")     
+        
+        
+        
